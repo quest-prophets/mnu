f5b216ba493ea0ddadc802a718bd99f5d630505b
sunDalik <dali1999@mail.ru>
2019-03-31 20:52:07 +0300

    registration... did something

--- src/main/kotlin/mnu/controller/AuthorizationController.kt
+++ src/main/kotlin/mnu/controller/AuthorizationController.kt
@@ -1,11 +1,13 @@
 package mnu.controller
 
+import mnu.form.RegistrationForm
 import mnu.model.User
 import mnu.model.enums.Role
 import mnu.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -20,47 +22,53 @@ class AuthorizationController {
     fun login() = "/index.html"
 
     @GetMapping("/register")
-    fun register() = "/register.html"
+    fun register(model: Model): String {
+        model.addAttribute("form", RegistrationForm())
+        return "/register.html"
+    }
 
     @GetMapping("/logout")
     fun logout() = "/index.html"
 
     @PostMapping("/register")
     @ResponseBody
-    fun addUser(@RequestBody user: User): AuthResponse {
-        val existingUser = userRepository?.findByLogin(user.login)
-        val authResponse = AuthResponse(user.login, AuthType.REGISTER)
-        val regex = """[a-zA-Z0-9_]+""".toRegex()
-
-        return if (!regex.matches(user.login) || !regex.matches(user.password)) {
-            authResponse.success = false
-            authResponse.message = "Only latin letters, numbers and underscore are supported."
-            authResponse
-        } else {
-
-            val passwordEncoder = BCryptPasswordEncoder()
-            val encodedPassword = passwordEncoder.encode(user.password)
-            user.password = encodedPassword
-
-            return if (existingUser != null) {
-                authResponse.success = false
-                authResponse.message = "Username '${user.login}' is already taken. Please try again."
-                authResponse
-            } else {
-
-//              user.active = true
-              //  user.role = Role.CLIENT
-                userRepository?.save(user)
-
-                authResponse.success = true
-                authResponse.message = "Successfully registered. You can sign in with your username and password."
-                authResponse
-            }
-        }
+    fun addUser(@ModelAttribute form: RegistrationForm): String {
+//        val existingUser = userRepository?.findByLogin(user.login)
+//        val authResponse = AuthResponse(user.login, AuthType.REGISTER)
+//        val regex = """[a-zA-Z0-9_]+""".toRegex()
+//
+//        return if (!regex.matches(user.login) || !regex.matches(user.password)) {
+//            authResponse.success = false
+//            authResponse.message = "Only latin letters, numbers and underscore are supported."
+//            authResponse
+//        } else {
+//
+//            val passwordEncoder = BCryptPasswordEncoder()
+//            val encodedPassword = passwordEncoder.encode(user.password)
+//            user.password = encodedPassword
+//
+//            return if (existingUser != null) {
+//                authResponse.success = false
+//                authResponse.message = "Username '${user.login}' is already taken. Please try again."
+//                authResponse
+//            } else {
+//
+////              user.active = true
+//              //  user.role = Role.CLIENT
+//                userRepository?.save(user)
+//
+//                authResponse.success = true
+//                authResponse.message = "Successfully registered. You can sign in with your username and password."
+//                authResponse
+//            }
+        //      }
+        return "redirect:index.html"
     }
 
     enum class AuthType { LOGIN, REGISTER }
 
-    data class AuthResponse(var login: String = "", var type: AuthType = AuthType.LOGIN,
-                            var success: Boolean = false, var message: String = "")
+    data class AuthResponse(
+        var login: String = "", var type: AuthType = AuthType.LOGIN,
+        var success: Boolean = false, var message: String = ""
+    )
 }
---
+++ src/main/kotlin/mnu/form/RegistrationForm.kt
@@ -0,0 +1,9 @@
+package mnu.form
+
+data class RegistrationForm(
+    val username: String = "",
+    val password: String = "",
+    val email: String = "",
+    val name: String = "",
+    val type: String = ""
+)
