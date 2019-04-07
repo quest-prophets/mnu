686e70fb99941b1dccf5dcd357af60af6b1028f0
sunDalik <dali1999@mail.ru>
2019-04-07 17:16:41 +0300

    fancy admin experiment requests handling

--- src/main/resources/static/js/administrators/admin__experiments.js
+++ src/main/resources/static/js/administrators/admin__experiments.js
@@ -5,13 +5,52 @@ document.addEventListener('click', (e) => {
     if (action === "accept") {
         fetch("/admin/acceptExperiment/" + experimentId, {method: 'POST'})
             .then(() => {
-                alert()
+                dealWithExperiment(true, experimentId);
             })
-    }
-    else if (action === "reject") {
+    } else if (action === "reject") {
         fetch("/admin/rejectExperiment/" + experimentId, {method: 'POST'})
             .then(() => {
-                alert()
+                dealWithExperiment(false, experimentId);
             })
     }
 });
+
+function dealWithExperiment(isAccept, experimentId) {
+    document.getElementById("exp-content-" + experimentId).classList.add("hidden");
+    document.getElementById("exp-flash-" + experimentId).classList.remove('hidden');
+    if (isAccept) {
+        document.getElementById("exp-header-" + experimentId).classList.add('approved');
+        document.getElementById("exp-header-" + experimentId).classList.remove('rejected');
+        document.getElementById("exp-header-" + experimentId).classList.remove('pending');
+        document.getElementById("exp-flash-message-" + experimentId).innerHTML = "Accepted";
+    } else {
+        document.getElementById("exp-header-" + experimentId).classList.add('rejected');
+        document.getElementById("exp-header-" + experimentId).classList.remove('approved');
+        document.getElementById("exp-header-" + experimentId).classList.remove('pending');
+        document.getElementById("exp-flash-message-" + experimentId).innerHTML = "Rejected";
+    }
+    let timeLeft = 9;
+    let undoButton = document.getElementById("exp-undo-" + experimentId);
+
+    let timer = setInterval(() => {
+        undoButton.innerHTML = "Undo " + timeLeft;
+        timeLeft--;
+    }, 1000);
+    let timeout = setTimeout(() => {
+        clearInterval(timer);
+        undoButton.parentNode.removeChild(undoButton);
+    }, 10000);
+    undoButton.addEventListener('click', () => {
+        clearInterval(timer);
+        clearTimeout(timeout);
+        fetch("/admin/undoExperimentChoice/" + experimentId, {method: 'POST'})
+            .then(() => {
+                document.getElementById("exp-content-" + experimentId).classList.remove("hidden");
+                document.getElementById("exp-flash-" + experimentId).classList.add('hidden');
+                document.getElementById("exp-header-" + experimentId).classList.add('pending');
+                document.getElementById("exp-header-" + experimentId).classList.remove('approved');
+                document.getElementById("exp-header-" + experimentId).classList.remove('rejected');
+                undoButton.innerHTML = "Undo 10";
+            })
+    });
+}
