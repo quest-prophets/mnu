2f8a892b8d7b8d077cd4a9a887cd09849bc980cc
sunDalik <dali1999@mail.ru>
2019-04-07 21:42:53 +0300

    adm/employees BETTER + date formatter

--- src/main/resources/static/js/administrators/admin__employees.js
+++ src/main/resources/static/js/administrators/admin__employees.js
@@ -1,22 +1,22 @@
-function setAddInterface() {
+document.getElementById("add-button").addEventListener('click', () => {
     pressButton(document.getElementById("add-button"));
     unhideInterface(document.getElementById("add-interface"));
-}
+});
 
-function setEditInterface() {
+document.getElementById("edit-button").addEventListener('click', () => {
     pressButton(document.getElementById("edit-button"));
     unhideInterface(document.getElementById("edit-interface"));
-}
+});
 
-function setFindInterface() {
+document.getElementById("find-button").addEventListener('click', () => {
     pressButton(document.getElementById("find-button"));
     unhideInterface(document.getElementById("find-interface"));
-}
+});
 
-function setRewardInterface() {
+document.getElementById("reward-button").addEventListener('click', () => {
     pressButton(document.getElementById("reward-button"));
     unhideInterface(document.getElementById("reward-interface"));
-}
+});
 
 function unhideInterface(unhidden_interface) {
     document.getElementById("add-interface").classList.add("hidden");
@@ -30,4 +30,14 @@ function pressButton(button_to_press) {
     let pressed_buttons = document.getElementsByClassName("button_pressed");
     while (pressed_buttons.length > 0) pressed_buttons[0].classList.remove("button_pressed");
     button_to_press.classList.add("button_pressed");
+}
+
+//Dates formatting
+dates = document.getElementsByClassName("td_date");
+for (let i = 0; i < dates.length; ++i) {
+    const date = new Date(dates[i].innerText);
+    const day = date.getDate().length === 2 ? date.getDate() : "0" + date.getDate();
+    const month = (date.getMonth() + 1).length === 2 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1);
+    const year = date.getFullYear();
+    dates[i].innerText = day + "." + month + "." + year;
 }
