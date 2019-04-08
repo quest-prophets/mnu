d172d67db5c92230dba66d4391b3383a319a224f
sunDalik <dali1999@mail.ru>
2019-04-08 19:25:32 +0300

    reports doooone

--- src/main/kotlin/mnu/form/NewReportForm.kt
+++ src/main/kotlin/mnu/form/NewReportForm.kt
@@ -5,10 +5,11 @@ data class NewReportForm(
     val result: String = "",
     val isSynthesized: String = "", // 0 - No; 1 - Yes, by Id; 2 - Brand-new weapon
     val weaponId: String = "", //if isSynthesized == 1
+    val weaponQuantity1: String = "",
     val weaponName: String = "", //this and all below if isSynthesized == 2
     val weaponType: String = "",
     val weaponDescription: String = "",
     val weaponPrice: String = "",
     val weaponLevel: String = "",
-    val weaponQuantity: String = ""
+    val weaponQuantity2: String = ""
 )
