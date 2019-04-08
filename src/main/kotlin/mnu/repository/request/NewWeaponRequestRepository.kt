40ff07c11b700bae03d25794c6884b03a20cf0f9
sunDalik <dali1999@mail.ru>
2019-04-08 19:42:39 +0300

    new experiment interface fix

--- src/main/kotlin/mnu/repository/request/NewWeaponRequestRepository.kt
+++ src/main/kotlin/mnu/repository/request/NewWeaponRequestRepository.kt
@@ -1,17 +1,18 @@
 package mnu.repository.request
 
 import mnu.model.Client
+import mnu.model.User
 import mnu.model.request.NewWeaponRequest
 import org.springframework.data.jpa.repository.*
 import org.springframework.data.repository.query.Param
 
 interface NewWeaponRequestRepository : JpaRepository<NewWeaponRequest, Long>{
-    fun findAllByClient(client: Client) : List<NewWeaponRequest>
+    fun findAllByUser(user: User) : List<NewWeaponRequest>
 
-    @Query("select r.id, nwr.requester_id, nwr.name, nwr.type, nwr.description, nwr.quantity, nwr.required_access_lvl from new_weapon_requests nwr" +
-            " inner join requests r on (nwr.request_id = r.id)" +
-            " inner join clients cl on (nwr.requester_id = cl.id)" +
-            " where ((cl.manager_id = ?1) and (r.status = 'PENDING'));", nativeQuery = true)
-    fun getAllPendingRequestsForManagerByClient(managerId: Long) : List<Array<Any>>
+//    @Query("select r.id, nwr.requester_id, nwr.name, nwr.type, nwr.description, nwr.quantity, nwr.required_access_lvl from new_weapon_requests nwr" +
+//            " inner join requests r on (nwr.request_id = r.id)" +
+//            " inner join clients cl on (nwr.requester_id = cl.id)" +
+//            " where ((cl.manager_id = ?1) and (r.status = 'PENDING'));", nativeQuery = true)
+//    fun getAllPendingRequestsForManagerByClient(managerId: Long) : List<Array<Any>>
 
 }
--- src/main/resources/static/js/scientists/sci__new-experiment.js
+++ src/main/resources/static/js/scientists/sci__new-experiment.js
@@ -2,21 +2,21 @@ document.getElementById("assistants-tbody").addEventListener('click', (e) => {
     const row = e.target.closest('[data-id]');
     if (!row) return;
 
-    const oldId = document.getElementById("assistant-id").value;
+    const oldId = document.getElementById("assistantId").value;
     const newId = row.dataset.id;
 
     if (oldId !== newId) {
         let selectedRows = document.getElementsByClassName("selected-row");
         while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
         row.classList.add("selected-row");
-        document.getElementById("assistant-id").value = newId;
+        document.getElementById("assistantId").value = newId;
     } else {
         row.classList.remove("selected-row");
-        document.getElementById("assistant-id").value = "";
+        document.getElementById("assistantId").value = "";
     }
 });
 
-document.getElementById("assistant-id").addEventListener('input', e => {
+document.getElementById("assistantId").addEventListener('input', e => {
     let selectedRows = document.getElementsByClassName("selected-row");
     while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
     let row =  document.querySelector('#assistants-tbody *[data-id="' + e.target.value + '"]');
