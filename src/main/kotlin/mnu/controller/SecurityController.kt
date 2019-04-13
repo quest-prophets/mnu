5692b9057446ece7c2d67e7f5e65b975afa87609
nailstorms <nailstorm@protonmail.ch>
2019-04-13 20:15:23 +0300

    crazy lol

--- src/main/kotlin/mnu/controller/SecurityController.kt
+++ src/main/kotlin/mnu/controller/SecurityController.kt
@@ -212,7 +212,7 @@ class SecurityController (
         val incident = districtIncidentRepository.findById(incidentId)
         if (!incident.isPresent)
             return "Incident with such id does not exist."
-        if (incident.get().dangerLevel > 0)
+        if (incident.get().availablePlaces > 0)
             return "You can not write a report on an incident which has not been resolved yet."
         if (!incident.get().assistants!!.contains(currentSecurity))
             return "You are not allowed to write a report on incident you did not participate in."
