2ee41136802045e05e07eae1d073a7f6028c0f39
sunDalik <dali1999@mail.ru>
2019-04-13 23:35:28 +0300

    change equipment better and wetter

--- src/main/resources/static/js/security/sec__equipment-change.js
+++ src/main/resources/static/js/security/sec__equipment-change.js
@@ -1,11 +1,53 @@
 document.getElementById("equipment-switch").addEventListener('change', e => {
-    const weaponRows = document.getElementsByClassName('weapon-rows');
-    const transportRows = document.getElementsByClassName('transport-rows');
     if (e.target.checked) {
-        for (let i = 0; i < weaponRows.length; ++i) weaponRows[i].classList.remove("hidden");
-        for (let i = 0; i < transportRows.length; ++i) transportRows[i].classList.add("hidden");
+        document.getElementById("weapons-tbody").classList.remove("hidden");
+        document.getElementById("transport-tbody").classList.add("hidden");
     } else {
-        for (let i = 0; i < weaponRows.length; ++i) weaponRows[i].classList.add("hidden");
-        for (let i = 0; i < transportRows.length; ++i) transportRows[i].classList.remove("hidden");
+        document.getElementById("weapons-tbody").classList.add("hidden");
+        document.getElementById("transport-tbody").classList.remove("hidden");
     }
+});
+
+document.getElementById("weapons-tbody").addEventListener('click', e => {
+    const row = e.target.closest('.weapon-row');
+    if (!row) return;
+    if (row.classList.contains("selected-row")) {
+        row.classList.remove("selected-row");
+        document.getElementById("weaponId").value = "";
+    } else {
+        let selectedRows = document.getElementsByClassName("selected-row");
+        while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
+        row.classList.add("selected-row");
+        document.getElementById("weaponId").value = row.dataset.id;
+    }
+});
+
+document.getElementById("transport-tbody").addEventListener('click', e => {
+    const row = e.target.closest('.transport-row');
+    if (!row) return;
+    if (row.classList.contains("selected-row")) {
+        row.classList.remove("selected-row");
+        document.getElementById("transportId").value = "";
+    } else {
+        let selectedRows = document.getElementsByClassName("selected-row");
+        while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
+        row.classList.add("selected-row");
+        document.getElementById("transportId").value = row.dataset.id;
+    }
+});
+
+document.getElementById("weaponId").addEventListener('input', e => {
+    let selectedRows = document.getElementsByClassName("selected-row");
+    while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
+    let row = document.querySelector('#weapons-tbody *[data-id="' + e.target.value + '"]');
+    if (!row) return;
+    row.classList.add("selected-row");
+});
+
+document.getElementById("transportId").addEventListener('input', e => {
+    let selectedRows = document.getElementsByClassName("selected-row");
+    while (selectedRows.length > 0) selectedRows[0].classList.remove("selected-row");
+    let row = document.querySelector('#transport-tbody *[data-id="' + e.target.value + '"]');
+    if (!row) return;
+    row.classList.add("selected-row");
 });
